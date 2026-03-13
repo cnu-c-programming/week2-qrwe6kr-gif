46194ee4c6f20096adc4884efd4a8c9db076d557
qrwe6kr-gif <qrwe6kr@gmail.com>
2026-03-13 17:21:16 +0900

    Update 4-2.c

--- code/4-2.c
+++ code/4-2.c
@@ -2,14 +2,11 @@
 
 int main()
 {
-int i = 1;
-int j = 1;
-for (int i=1; i<=0; i++) {
+for (int i=1; i<=9; i++) {
      for (int j=1; j<=9; j++) {
-        printf("%d * %d\n = %d", i*j);
+        printf("%d * %d = %d\n", i, j, i*j);
      }
 }
 
     return 0;
 }
-
