8b9dba32735d3c4b5bd22e8e6e64c39622b729cc
qrwe6kr-gif <qrwe6kr@gmail.com>
2026-03-13 17:08:15 +0900

    Update 2-1.c

--- code/2-1.c
+++ code/2-1.c
@@ -3,12 +3,12 @@
 int main()
 {
     printf("char: %u\n", sizeof(char));
-    printf("Short: %u\n", size(short));
+    printf("Short: %u\n", sizeof(short));
     printf("int: %u\n", sizeof(int));
-    printf("unsigned int: %u\n", size(unsigned int));
+    printf("unsigned int: %u\n", sizeof(unsigned int));
     printf("long: %u\n", sizeof(long));
     printf("long long: %u\n", sizeof(long  long));
-    printf("float: %u\n", size(long long));
+    printf("float: %u\n", sizeof(long long));
 
     return 0;
 }
