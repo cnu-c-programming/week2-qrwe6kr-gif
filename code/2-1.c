911d67692bb54c49a287a0cac9cd76cc65dc4154
qrwe6kr-gif <qrwe6kr@gmail.com>
2026-03-13 09:44:50 +0900

    Update 2-1.c

--- code/2-1.c
+++ code/2-1.c
@@ -2,6 +2,20 @@
 
 int main()
 {
+    int var1 = 100;
+    printf("int : %d\n", var1);
+    printf("hex : %d\n", var1);
+    printf("oct : %d\n", var1);
+
+    char var2 = 'c';
+    printf("char : %c\n", var2);
+
+    char* var3 = "hello world!";
+    printf("string : %s\n", var3);
+
+    float var4 = 3.141592;
+    printf("float : %f\n", var4);
+    printf("exp : %1f\n", var4);
 
     return 0;
 }
