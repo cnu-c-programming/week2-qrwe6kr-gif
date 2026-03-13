827d07f5a5dd92de2e594bee9202581ddac4c126
qrwe6kr-gif <qrwe6kr@gmail.com>
2026-03-13 17:27:17 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -6,13 +6,16 @@ int main()
     scanf("%d", &x);
 
     if ( x > 1 ) {
-        for ( int i=1; i < x; i++ ) {
+        for ( int i=2; i < x; i++ ) {
             if ( x % i == 0 ) {
-                printf("%s", "false");
+                printf("%s\n", "false");
+                break;
             }
-            else {
-                printf("%s", "true"); 
+            else { 
+                 printf("%s\n", "true");
+                break;
             }
+        
         }
     }
     return 0;
