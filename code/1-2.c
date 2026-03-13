4ed3fa56f88bb96bed2a94aca5f84eff5a252294
qrwe6kr-gif <qrwe6kr@gmail.com>
2026-03-13 16:38:52 +0900

    Update 1-2.c

--- code/1-2.c
+++ code/1-2.c
@@ -6,16 +6,16 @@ int main()
     printf("% 11s\n", "value");
 
     printf("% 4s", "kim");
-    printf("% 11d", "12");
-    printf("% 11f\n", "4.120000");
+    printf("% 11d", 12);
+    printf("% 11f\n", 4.120000);
 
     printf("% 4s", "lee");
-    printf("% 11d", "1922");
-    printf("% 11f\n", "1.000000");
+    printf("% 11d", 1922);
+    printf("% 11f\n", 1.000000);
 
     printf("% 4s", "park");
-    printf("% 11d", "432");
-    printf("% 11f\n", "1.000000");
+    printf("% 11d", 432);
+    printf("% 11f\n", 1.000000);
     
     return 0;
 }
