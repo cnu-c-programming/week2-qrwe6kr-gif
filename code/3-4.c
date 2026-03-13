672caf65945a05672ed816c6cd4bd2a63925c0ad
qrwe6kr-gif <qrwe6kr@gmail.com>
2026-03-13 21:44:58 +0900

    Update 3-4.c

--- code/3-4.c
+++ code/3-4.c
@@ -3,14 +3,14 @@
 int main()
 {
     int a,b,c;
-
+    
     a = b = c = 5;
     prinf("%d %d %d\n", a, b, c);
 
     a = 2 + 3 * 4;
     prinf("%d %d %d\n", a, b, c);
 
-    c = a++ + ++ b;
+    c = a++ + ++b;
     prinf("%d %d %d\n", a, b, c);
 
     return 0;
