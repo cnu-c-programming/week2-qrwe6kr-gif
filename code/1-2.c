8de003fc282da32cc67a527e9df658f4e00db2a9
qrwe6kr-gif <qrwe6kr@gmail.com>
2026-03-13 09:54:36 +0900

    Update 1-2.c

--- code/1-2.c
+++ code/1-2.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 int main()
 {
+    printf("% 10s\n", "name id value");
+    printf("% 10s\n", "kim 12 4. 120000");
+    printf("% 10s\n", "lee 1922 3.210000");
+    printf("% 10s\n", "park 432 1.000000");
 
 
     return 0;
