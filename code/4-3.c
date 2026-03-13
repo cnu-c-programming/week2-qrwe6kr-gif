43db9c9bfe2d914107b00bd6620380b79d8adfc6
qrwe6kr-gif <qrwe6kr@gmail.com>
2026-03-13 15:56:06 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -2,7 +2,19 @@
 
 int main()
 {
+    int x;
+    scanf("%d", &x);
 
+    if ( x > 1 ) {
+        for ( int i=1; i < x; i++ ) {
+            if ( x % i == 0 ) {
+                printf("%s", "false");
+            }
+            else {
+                printf("%s", "true"); 
+            }
+        }
+    }
     return 0;
 }
 
