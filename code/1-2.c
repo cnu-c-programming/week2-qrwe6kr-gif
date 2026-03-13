f892d86159e1e2f108552de938f7d46149f3f413
qrwe6kr-gif <qrwe6kr@gmail.com>
2026-03-13 17:03:04 +0900

    Update 1-2.c

--- code/1-2.c
+++ code/1-2.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 int main()
 {
-    printf("% 4s %10s % 10s\n", "name", "id", "value");
+    printf("% 10s %10s % 10s\n", "name", "id", "value");
     
-    printf("% 4s %010d %10.2f\n", "kim", 12, 4.120000);
+    printf("% 10s %010d %10.2f\n", "kim", 12, 4.120000);
     
-    printf("% 4s %010d %10.2f\n", "lee", 1922, 3.210000);
+    printf("% 10s %010d %10.2f\n", "lee", 1922, 3.210000);
     
-    printf("% 4s %010d %10.2f\n", "park", 432, 1.000000);
+    printf("% 10s %010d %10.2f\n", "park", 432, 1.000000);
 
     return 0;
 }
